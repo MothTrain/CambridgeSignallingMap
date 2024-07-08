1a5691c28d2a81c25ff210f45c5284a90e4e5c81
MothTrain <aradgnezami@gmail.com>
2024-07-08 20:30:49 +0100

    Testing dependencies added

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,8 +2,9 @@ module aradnezami.cambridgesignallingmap {
     requires javafx.controls;
     requires javafx.fxml;
     requires org.apache.logging.log4j;
+    requires org.apache.logging.log4j.core;
     
     
-    opens aradnezami.cambridgesignallingmap to javafx.fxml;
-    exports aradnezami.cambridgesignallingmap;
+    /*opens aradnezami.cambridgesignallingmap to javafx.fxml;
+    exports aradnezami.cambridgesignallingmap;*/
 }
