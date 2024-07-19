ef23dc04bc95facc6e983af77934983fe520f6aa
MothTrain <aradgnezami@gmail.com>
2024-07-19 13:05:34 +0100

    Route class added to Diagrams

---
+++ src/main/java/aradnezami/cambridgesignallingmap/Diagram/Route.java
@@ -0,0 +1,8 @@
+package aradnezami.cambridgesignallingmap.Diagram;
+
+public class Route {
+    public static int TYPE = 4;
+    
+    public static int SET = 0;
+    public static int NOTSET = 1;
+}
