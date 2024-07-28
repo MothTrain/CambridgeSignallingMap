b3c5cb5b1f76b3e4cc345865978c8647e69d389d
MothTrain <aradgnezami@gmail.com>
2024-07-28 18:45:11 +0100

    Fix: translateState added to route class

--- src/main/java/aradnezami/cambridgesignallingmap/Diagram/Route.java
+++ src/main/java/aradnezami/cambridgesignallingmap/Diagram/Route.java
@@ -5,4 +5,12 @@ public class Route {
     
     public static final int SET = 0;
     public static final int NOTSET = 1;
+    
+    public static String translateState(int state) {
+        return switch (state) {
+            case SET -> "Set";
+            case NOTSET -> "Not Set";
+            default -> throw new IllegalArgumentException(state + " is not a valid Route state");
+        };
+    }
 }
