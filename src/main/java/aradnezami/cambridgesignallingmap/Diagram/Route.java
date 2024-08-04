452480995afadd1cd2e080826bfcbaafe3d6b2e3
MothTrain <aradgnezami@gmail.com>
2024-08-04 17:18:37 +0100

    Call-on, shunt and main routes added to route class

--- src/main/java/aradnezami/cambridgesignallingmap/Diagram/Route.java
+++ src/main/java/aradnezami/cambridgesignallingmap/Diagram/Route.java
@@ -1,7 +1,9 @@
 package aradnezami.cambridgesignallingmap.Diagram;
 
 public class Route {
-    public static final int TYPE = 4;
+    public static final int MAIN_TYPE = 4;
+    public static final int SHUNT_TYPE = 5;
+    public static final int CALL_ON_TYPE = 6;
     
     public static final int SET = 0;
     public static final int NOTSET = 1;
