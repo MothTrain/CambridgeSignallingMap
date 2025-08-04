eb65217d502cb100f6e05faefafb04f5f0851d84
MothTrain <aradgnezami@gmail.com>
2025-08-04 17:08:34 +0100

    NRFeed: added + test

---
+++ src/main/java/aradnezami/cambridgesignallingmap/NRFeed/NRFeed.java
@@ -0,0 +1,119 @@
+package aradnezami.cambridgesignallingmap.NRFeed;
+
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
+/**
+ * The NRFeedClient is used by users of the package to access the Network Rail data feeds and returns
+ * {@link Event}s in a decoded form.
+ */
+public class NRFeed {
+    public static void main(String[] args) {
+        NRFeedClient nrFeedClient = new DataServerNRClient(DataServerNRClient.getSocket("Cam-PSB-Server",65432));
+        SClassDecoder sClassDecoder;
+        try {
+            sClassDecoder = new SClassDecoder("SignallingEquipmentMap.csv");
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        NRFeed feed = new NRFeed(nrFeedClient, sClassDecoder);
+
+        while (feed.isAlive()) {
+            System.out.println(feed.nextEvent());
+        }
+    }
+
+
+    private final SClassDecoder decoder;
+    private final NRFeedClient client;
+
+    private final ArrayList<Event> bufferedEvents = new ArrayList<>();
+
+
+    public final int TYPE = 0;
+    public final int TIMESTAMP = 1;
+
+    public final int S_ADDRESS = 2;
+    public final int S_BYTE = 3;
+
+    public final int C_FROMBERTH = 2;
+    public final int C_TOBERTH = 3;
+    public final int C_DESCRIBER = 4;
+
+
+    /**
+     * Creates an instance of NRFeed using the provided NRFeedClient as the data feed and
+     * the decoder for understanding S-Class messages. The instance becomes responsible for
+     * handling the client, so the user should not interact with the client hereafter
+     * @param client NRFeedClient to be used
+     * @param decoder SClassDecoder to be used
+     */
+    NRFeed(NRFeedClient client, SClassDecoder decoder) {
+        this.decoder = decoder;
+        this.client = client;
+    }
+
+
+    /**
+     * Polls for the next {@link Event} from the feed. If no event is available, the method
+     * blocks until one is received. If an S-Class event constitutes several changes, the
+     * method will return the events sequentially in the order of the bit number in the mappings.
+     * @return The most recently received event from the feed
+     * @throws NRFeedException If a connection error occurs
+     */
+    public Event nextEvent() throws NRFeedException {
+        if (!bufferedEvents.isEmpty()) {
+            Event event = bufferedEvents.get(0);
+            bufferedEvents.remove(0);
+            return event;
+        }
+
+        String[] eventStr = client.pollNREvent().split(",");
+        long timestamp = Long.parseLong(eventStr[TIMESTAMP]);
+
+        if (eventStr[TYPE].equals("C")) {
+            return new Event(timestamp, eventStr[C_FROMBERTH], eventStr[C_TOBERTH], eventStr[C_DESCRIBER]);
+        }
+
+        int address = Integer.parseInt(eventStr[S_ADDRESS], 16);
+        int data = Integer.parseInt(eventStr[S_BYTE], 16);
+        Event[] events = decoder.SClassChange(timestamp, address, data);
+
+        if (events.length == 0) {
+            return nextEvent();
+        } else if (events.length == 1) {
+            return events[0];
+        } else {
+            bufferedEvents.addAll(Arrays.asList(events).subList(1, events.length)); // buffer all *except 1st event*
+            return events[0];
+        }
+    }
+
+    /**
+     *  Resets all SClass state in the instance. The instance will behave as though
+     *  it has just been constructed with no knowledge of any S-Class state.
+     */
+    public void reset() {
+        decoder.reset();
+    }
+
+
+    /**
+     * Checks if this instance is alive. If false is returned, all subsequent calls to {@link #nextEvent()}
+     * will throw {@link NRFeedException}
+     * @return True if instance is alive, false otherwise
+     */
+    public boolean isAlive() {
+        return client.isAlive();
+    }
+
+    /**
+     * Disconnects the instance from the feed
+     */
+    public void disconnect() {
+        client.disconnect();
+    }
+}
---
+++ src/test/java/aradnezami/cambridgesignallingmap/NRFeed/NRFeedTest.java
@@ -0,0 +1,129 @@
+package aradnezami.cambridgesignallingmap.NRFeed;
+
+import aradnezami.cambridgesignallingmap.Diagram.Point;
+import aradnezami.cambridgesignallingmap.Diagram.Signal;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
+
+class NRFeedTest {
+    private NRFeed feed;
+
+    @Mock
+    private NRFeedClient client;
+    @Mock
+    private SClassDecoder decoder;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        feed = new NRFeed(client, decoder);
+    }
+
+
+    @Test
+    @DisplayName("nextEvent(): one SClass Event decoded")
+    void nextEvent1() {
+        Event expected1 = new Event(-1L, Signal.TYPE, Signal.MAIN_OFF, "123");
+        Event expected2 = new Event(-1L, Signal.TYPE, Signal.MAIN_OFF, "456");
+        when(client.pollNREvent()).thenReturn("s,1,2,3,4,5"); // Value doesn't matter
+        when(decoder.SClassChange(anyLong(), anyInt(), anyInt()))
+                .thenReturn(new Event[]{expected1})
+                .thenReturn(new Event[]{expected2}); // Second value just ensures a value isn't returned again
+
+        assertEquals(expected1, feed.nextEvent());
+        assertEquals(expected2, feed.nextEvent());
+        verify(client, times(2)).pollNREvent();
+        verify(decoder, times(2)).SClassChange(anyLong(), anyInt(), anyInt());
+    }
+
+    @Test
+    @DisplayName("nextEvent(): multiple SClass events decoded")
+    void nextEvent2() {
+
+        Event expected1 = new Event(-1L, Signal.TYPE, Signal.MAIN_OFF, "123");
+        Event expected2 = new Event(-1L, Signal.TYPE, Signal.MAIN_OFF, "456");
+        Event expected3 = new Event(-1L, Point.TYPE, Point.REVERSE, "4567");
+
+        when(client.pollNREvent()).thenReturn("s,1,2,3,4,5"); // Value doesn't matter
+        when(decoder.SClassChange(anyLong(), anyInt(), anyInt()))
+                .thenReturn(new Event[]{expected1, expected2})
+                .thenReturn(new Event[]{expected3});
+
+
+        assertEquals(expected1, feed.nextEvent());
+        assertEquals(expected2, feed.nextEvent());
+        assertEquals(expected3, feed.nextEvent());
+        verify(client, times(2)).pollNREvent();
+        verify(decoder, times(2)).SClassChange(anyLong(), anyInt(), anyInt());
+    }
+
+    @Test
+    @DisplayName("nextEvent(): no SClass Event decoded")
+    void nextEvent3() {
+        Event expected = new Event(-1L, Signal.TYPE, Signal.MAIN_OFF, "123");
+        when(client.pollNREvent()).thenReturn("s,1,2,3,4,5"); // Value doesn't matter
+        when(decoder.SClassChange(anyLong(), anyInt(), anyInt()))
+                .thenReturn(new Event[]{}) // No event decoded
+                .thenReturn(new Event[]{expected}); // Second value just ensures a value isn't returned again
+
+        assertEquals(expected, feed.nextEvent());
+        verify(client, times(2)).pollNREvent();
+        verify(decoder, times(2)).SClassChange(anyLong(), anyInt(), anyInt());
+    }
+
+    @Test
+    @DisplayName("nextEvent(): C-Class Event")
+    void nextEvent4() {
+        Event expected = new Event(-1L, "0123", "0456", "1K67");
+        when(client.pollNREvent()).thenReturn("C,-1,0123,0456,1K67");
+
+        assertEquals(expected, feed.nextEvent());
+        verifyNoInteractions(decoder);
+    }
+
+    @Test
+    @DisplayName("nextEvent(): NRFeedException")
+    void nextEvent5() {
+        when(client.pollNREvent()).thenThrow(new NRFeedException("msg", "Display msg"));
+
+        assertThrows(NRFeedException.class, () -> feed.nextEvent());
+    }
+
+
+    @Test
+    void disconnect() {
+        feed.disconnect();
+
+        verify(client, times(1)).disconnect();
+        // We dont need to check that nextEvent() throws an exception. This behaviour is managed by the NRFeedClient.
+        // Any further testing of isalive or nextEvent, would simply be testing the NRFeedClient
+    }
+
+    @Test
+    void isAlive() {
+        when(client.isAlive())
+                .thenReturn(true)
+                .thenReturn(false);
+
+        assertTrue(feed.isAlive());
+        assertFalse(feed.isAlive());
+
+        verify(client, times(2)).isAlive();
+    }
+
+
+    @Test
+    void reset() {
+        feed.reset();
+
+        verify(decoder, times(1)).reset();
+    }
+}
