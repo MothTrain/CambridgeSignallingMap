2fdff63bf88a64cc64a12b944398c35c54db6c86
MothTrain <aradgnezami@gmail.com>
2025-08-10 22:29:06 +0100

    DatumPoint: Temporary skeleton class

---
+++ src/main/java/aradnezami/cambridgesignallingmap/DiagramElements/DatumPoint.java
@@ -0,0 +1,15 @@
+package aradnezami.cambridgesignallingmap.DiagramElements;
+
+/**
+ * A temporary skeleton for the datum point class, simply storing a position, which relevant elements
+ * may recall.
+ */
+public class DatumPoint {
+    public int x;
+    public int y;
+
+    public DatumPoint(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
