e969c2cfef9d8c6f5f3b41fbed1615775ce2d451
MothTrain <aradgnezami@gmail.com>
2025-08-12 14:25:17 +0100

    TrackCircuit: Now functional

--- src/main/java/aradnezami/cambridgesignallingmap/DiagramElements/TrackCircuit.java
+++ src/main/java/aradnezami/cambridgesignallingmap/DiagramElements/TrackCircuit.java
@@ -1,5 +1,15 @@
 package aradnezami.cambridgesignallingmap.DiagramElements;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.awt.*;
+
+/**
+ * A track circuit holds a series of tracks and sets them as occupied or unoccupied . The
+ * tracks may be modified after construction of the route, however if any tracks are overwritten, the
+ * {@link #setTracks(Track[])} method must be used. Note that any changes to state will not apply until
+ * the Track's {@link Track#draw(Graphics2D)} method is called
+ */
 public class TrackCircuit {
     /**
      * Used to indicate the event type
@@ -8,6 +18,51 @@ public class TrackCircuit {
     
     public static final int UNOCCUPIED = 0;
     public static final int OCCUPIED = 1;
+
+    @NotNull
+    public String name;
+    @NotNull
+    public String datumName;
+
+    @NotNull
+    private Track[] tracks;
+    private boolean isOccupied;
+
+    /**
+     * Creates a track circuit with the following properties
+     * @param name Unique name of the track circuit
+     * @param datumName Name of datum point. Since the track circuit has no location, the datum point is
+     *                  only for organisation
+     * @param tracks The tracks covered by the track circuit
+     */
+    public TrackCircuit(@NotNull String name,
+                        @NotNull String datumName,
+                        @NotNull Track[] tracks) {
+
+        this.name = name;
+        this.datumName = datumName;
+        this.tracks = tracks;
+        isOccupied = false;
+    }
+
+    public void setState(int state) {
+        switch (state) {
+            case UNOCCUPIED -> isOccupied = false;
+            case OCCUPIED -> isOccupied = true;
+            default -> throw new IllegalStateException("Unknown track circuit state: " + state + " TC=" + name);
+        }
+
+        for (Track track : tracks) {
+            track.setOccupied(isOccupied);
+        }
+    }
+
+    public Track[] getTracks() {
+        return tracks;
+    }
+    public void setTracks(Track[] tracks) {
+        this.tracks = tracks;
+    }
     
     /**
      * Returns a string representation of the track circuit state <br>
